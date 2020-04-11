b80a7b81dcb564897db00b770399503795c406c9
Omar Nabi <onabi20@gmail.com>
2020-04-11 05:25:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -49,17 +49,5 @@ def self.artist_count
       end
       artist_count
     end
-end
 
-def self.artist_count
-    artist_count = {}
-    @@artists.each do |artist|
-      if artist_count[artist] 
-        artist_count[artist] += 1
-      else
-        artist_count[artist] = 1
-      end
-    end
-    artist_count
-  end
-end
+

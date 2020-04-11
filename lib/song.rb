c0b37e989a99ee4e6c4592f939a2920d25e6a48f
Omar Nabi <onabi20@gmail.com>
2020-04-11 04:00:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,15 @@
 class Song 
   attr_accessor :name :artist :genre 
-  @@count
+  @@count = 0 
+  
+  def initialize.count
+    self.count += 1 
+  end
+  
+  def count 
+    @@count
+  end
+  
   
 
     

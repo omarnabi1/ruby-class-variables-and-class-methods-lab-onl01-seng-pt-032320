52834562b3c1c65376fe0913fb867b77466e244f
Omar Nabi <onabi20@gmail.com>
2020-04-11 05:06:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,18 +35,7 @@ class Song
         genre_count[genre] = 1
       end
     end
-    genre_count
+    genre_count #why is the class method being called in our code?
   end
 
-  def self.artist_count
-    artist_count = {}
-    @@artists.each do |artist|
-      if artist_count[artist] #if the artist is already in the hash
-        artist_count[artist] += 1
-      else
-        artist_count[artist] = 1
-      end
-    end
-    artist_count
-  end
-end
+  

95a58ce9baa8be74d6550466d8a57bb5674d775f
Omar Nabi <onabi20@gmail.com>
2020-04-11 05:21:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,13 +40,13 @@ class Song
 
 def self.artist_count
     artist_count = {}
-    @@artists.each do |artist|
-      if artist_count[artist] #if the artist is already in the hash
-        artist_count[artist] += 1
-      else
-        artist_count[artist] = 1
+     @@artists.each do |artist|
+        if artist_count[artist]
+          artist_count[artist] =+ 1 
+        else 
+          artist_count[artist] = 1 
+        end
       end
+      artist_count
     end
-    artist_count
-  end
 end

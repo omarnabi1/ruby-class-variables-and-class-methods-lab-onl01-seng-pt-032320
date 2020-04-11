f662a69511e370e4f0415b838255b15442477a09
Omar Nabi <onabi20@gmail.com>
2020-04-11 05:01:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,3 +29,12 @@ class Song
   genre = { }
   @@genres.each do |genre|
     if genre_count [genre]
+      genre_count [genre] += 1 #if the genre has already been recorded then dont create a new genre add 1 to the previously added genre_count
+    else #if it is a new genre then create a new genre count? (confused here)
+      genre_count [genre] = 1 
+    end
+  end
+  genre_count    #why is the class method being called in our code?
+end
+
+def
